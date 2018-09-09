45714170ca89ad964f1bf4b86fa1fac943be6a95
Arti M <artismarti@gmail.com>
2018-09-09 19:10:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num%2 == 0
+  if num%2 == 0 && num%5 == 0
     puts "Fizz"
 end
